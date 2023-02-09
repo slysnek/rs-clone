ba8affd54c66d08da50b24079cdd9e7b5148962b
Pavel Prokopenya <pavel12minsk@mail.ru>
2023-02-09 15:44:25 +0300

    feat: change spritesheets & animations

--- src/game/game.ts
+++ src/game/game.ts
@@ -2,21 +2,96 @@ import Phaser, { Tilemaps } from 'phaser';
 import { Direction, GridEngine } from 'grid-engine';
 import windowSize from './constants';
 
-const startPositionsForEnemy: { [key: string]: { x: number, y: number } } = { 
-  enemy1: { x: 20, y: 34 },
-  enemy2: { x: 23, y: 36 }
+const startPositionsForEnemy: { [key: string]: { x: number, y: number } } = {
+  scorpion1: { x: 20, y: 34 },
+  scorpion2: { x: 23, y: 36 }
 };
 
-function getRandomXYDelta(){
+function getRandomXYDelta() {
   const deltaValue = () => Math.ceil(Math.random() * 10 / 3);
-  return {xDelta: deltaValue(), yDelta: deltaValue()};
+  return { xDelta: deltaValue(), yDelta: deltaValue() };
 }
 
 const timeModifier = 5;
 
-function getRandomTimeInterval(){
+function getRandomTimeInterval() {
   return (Math.ceil(Math.random() * timeModifier) * 1000);
 }
+
+type Animations = {
+  walk: {
+    upRight: {
+      startFrame: number;
+      endFrame: number;
+      stopFrame: number;
+    },
+    downRight: {
+      startFrame: number;
+      endFrame: number;
+      stopFrame: number;
+    },
+    downLeft: {
+      startFrame: number;
+      endFrame: number;
+      stopFrame: number;
+    },
+    upLeft: {
+      startFrame: number;
+      endFrame: number;
+      stopFrame: number;
+    }
+  }
+}
+
+const heroAnims = {
+  walk: {
+    upRight: {
+      startFrame: 0,
+      endFrame: 7,
+      stopFrame: 10,
+    },
+    downRight: {
+      startFrame: 11,
+      endFrame: 18,
+      stopFrame: 21,
+    },
+    downLeft: {
+      startFrame: 22,
+      endFrame: 29,
+      stopFrame: 32,
+    },
+    upLeft: {
+      startFrame: 33,
+      endFrame: 40,
+      stopFrame: 43,
+    },
+  },
+};
+
+const scorpionAnims = {
+  walk: {
+    upRight: {
+      startFrame: 0,
+      endFrame: 7,
+      stopFrame: 0,
+    },
+    downRight: {
+      startFrame: 16,
+      endFrame: 23,
+      stopFrame: 16,
+    },
+    downLeft: {
+      startFrame: 24,
+      endFrame: 31,
+      stopFrame: 24,
+    },
+    upLeft: {
+      startFrame: 40,
+      endFrame: 47,
+      stopFrame: 40,
+    },
+  },
+};
 
 class Game extends Phaser.Scene {
   hero: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
@@ -27,7 +102,7 @@ class Game extends Phaser.Scene {
   enemiesMovesTimers: { [enemyId: string]: NodeJS.Timer }
 
   constructor(hero: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody, cursors: Phaser.Types.Input.Keyboard.CursorKeys, gridEngine: GridEngine) {
-    super('game'); // why and how this works?
+    super('game');
     this.hero = hero;
     this.entitiesMap = new Map();
     this.cursors = cursors;
@@ -39,9 +114,9 @@ class Game extends Phaser.Scene {
   preload() {
     this.load.tilemapTiledJSON('map', 'assets/maps/isometric3.json');
     this.load.image('tiles', 'assets/maps/grassland_tiles.png');
-    this.load.spritesheet('player', 'assets/spritesheets/woman-01.png', { frameWidth: 75, frameHeight: 133 });
-    this.load.spritesheet('enemy1', 'assets/spritesheets/rad-scorpion-walk.png', { frameWidth: 120, frameHeight: 100 });
-    this.load.spritesheet('enemy2', 'assets/spritesheets/rad-scorpion-walk.png', { frameWidth: 120, frameHeight: 100 });
+    this.load.spritesheet('player', 'assets/spritesheets/woman-13-spritesheet.png', { frameWidth: 75, frameHeight: 133 });
+    this.load.spritesheet('scorpion1', 'assets/spritesheets/rad-scorpion-walk.png', { frameWidth: 120, frameHeight: 100 });
+    this.load.spritesheet('scorpion2', 'assets/spritesheets/rad-scorpion-walk.png', { frameWidth: 120, frameHeight: 100 });
   }
 
   create() {
@@ -49,13 +124,13 @@ class Game extends Phaser.Scene {
     this.tintTiles(map);
     this.createHero();
     this.createCamera();
-    this.createEnemy('enemy1');
-    this.createEnemy('enemy2', 0.7);
-    this.setFramesForEntitiesAnimations();
+    this.createEnemy('scorpion1');
+    this.createEnemy('scorpion2', 0.7);
+    this.setFramesForEntitiesAnimations(this.entitiesMap);
     this.cursors = this.input.keyboard.createCursorKeys();
     this.gridEngineInit(map);
     this.entitiesMap.forEach((entityValue, entityKey) => {
-      if(entityKey !== 'player'){
+      if (entityKey !== 'player') {
         this.setEnemyWalkBehavior(entityKey, map);
       }
     })
@@ -64,10 +139,10 @@ class Game extends Phaser.Scene {
   }
 
   update() {
-   this.moveHeroByArrows();
+    this.moveHeroByArrows();
   }
 
-  buildMap(){
+  buildMap() {
     const map = this.make.tilemap({ key: 'map' });
     const tilesets = map.addTilesetImage('grassland_tiles', 'tiles');
 
@@ -78,32 +153,32 @@ class Game extends Phaser.Scene {
     return map;
   }
 
-  createHero(){
+  createHero() {
     this.hero = this.physics.add.sprite(0, 0, 'player');
-    this.hero.scale = 0.75;
+    this.hero.scale = 1.5;
     this.entitiesMap.set('player', this.hero);
   }
 
-  createEnemy(key: string, scaleValue = 1){
+  createEnemy(key: string, scaleValue = 1) {
     const enemy = this.physics.add.sprite(0, 0, `${key}`);
     this.entitiesMap.set(`${key}`, enemy);
     enemy.scale = scaleValue;
   }
 
-  createCamera(){
+  createCamera() {
     this.cameras.main.setSize(windowSize.windowWidth, windowSize.windowHeight);
     this.cameras.main.startFollow(this.hero, true);
   }
 
-  gridEngineInit(map: Tilemaps.Tilemap){
+  gridEngineInit(map: Tilemaps.Tilemap) {
     const gridEngineConfig = {
       characters: [
         {
           id: "player",
           sprite: this.hero,
           startPosition: { x: 35, y: 28 },
-          offsetX: 5,
-          offsetY: -10,
+          offsetX: 0,
+          offsetY: 42,
           walkingAnimationEnabled: false,
           speed: 7,
         },
@@ -111,7 +186,7 @@ class Game extends Phaser.Scene {
       numberOfDirections: 4
     };
     this.entitiesMap.forEach((enemyValue, enemyKey) => {
-      if(enemyKey !== 'player'){
+      if (enemyKey !== 'player') {
         gridEngineConfig.characters.push(
           {
             id: enemyKey,
@@ -128,7 +203,7 @@ class Game extends Phaser.Scene {
     this.gridEngine.create(map, gridEngineConfig);
   }
 
-  subscribeCharacterToChangeMoving(){
+  subscribeCharacterToChangeMoving() {
     // Hero movements subscribers
     this.gridEngine.movementStarted().subscribe(({ charId, direction }) => {
       const entity = this.entitiesMap.get(charId) as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
@@ -138,16 +213,16 @@ class Game extends Phaser.Scene {
     this.gridEngine.movementStopped().subscribe(({ charId, direction }) => {
       const entity = this.entitiesMap.get(charId) as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
       entity.anims.stop();
-      entity.setFrame(this.getStopFrame(direction));
+      entity.setFrame(this.getStopFrame(direction, charId));
     });
 
     this.gridEngine.directionChanged().subscribe(({ charId, direction }) => {
       const entity = this.entitiesMap.get(charId) as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
-      entity.setFrame(this.getStopFrame(direction));
+      entity.setFrame(this.getStopFrame(direction, charId));
     });
   }
 
-  setPointerDownListener(map: Tilemaps.Tilemap){
+  setPointerDownListener(map: Tilemaps.Tilemap) {
     // Moving on mouse click
     this.input.on('pointerdown', () => {
       // Converting world coords into tile coords
@@ -165,18 +240,17 @@ class Game extends Phaser.Scene {
   }
 
   // позже надо удалить из аргументов карту и функцию покраски тайлов
-
-  setEnemyWalkBehavior(charId: string, map: Tilemaps.Tilemap){
+  setEnemyWalkBehavior(charId: string, map: Tilemaps.Tilemap) {
     this.enemiesMovesTimers.charId = setInterval(() => {
       const deltaXY = getRandomXYDelta();
-      this.gridEngine.moveTo(`${charId}`, { x: startPositionsForEnemy[charId].x + deltaXY.xDelta, y: startPositionsForEnemy[charId].y + deltaXY.yDelta } );
+      this.gridEngine.moveTo(`${charId}`, { x: startPositionsForEnemy[charId].x + deltaXY.xDelta, y: startPositionsForEnemy[charId].y + deltaXY.yDelta });
       this.tintTile(map, startPositionsForEnemy[charId].x + deltaXY.xDelta, startPositionsForEnemy[charId].y + deltaXY.yDelta, 0xff7a4a);
 
     }, getRandomTimeInterval())
   }
 
-  moveHeroByArrows(){
- // Move hero by arrows (can be deleted?)
+  moveHeroByArrows() {
+    // Move hero by arrows (can be deleted?)
     if (this.cursors.left.isDown) {
       this.gridEngine.move("player", Direction.UP_LEFT);
     } else if (this.cursors.right.isDown) {
@@ -188,14 +262,25 @@ class Game extends Phaser.Scene {
     }
   }
 
-  setFramesForEntitiesAnimations(){
-    console.log(this.entitiesMap)
-    this.entitiesMap.forEach((entityValue, entityKey) => {
-      this.createEntityAnimation.call(entityValue, "up-right", entityKey, 0, 7);
-      this.createEntityAnimation.call(entityValue, "down-right", entityKey, 16, 23);
-      this.createEntityAnimation.call(entityValue, "down-left", entityKey, 24, 31);
-      this.createEntityAnimation.call(entityValue, "up-left", entityKey, 40, 47);
-    })
+  setFramesForEntitiesAnimations(entitiesMap: Map<string, Phaser.Types.Physics.Arcade.SpriteWithDynamicBody>) {
+    const heroRegex = /^player/i;
+    const scorpionRegex = /^scorpion/i;
+
+    entitiesMap.forEach((entityValue, entityKey) => {
+      if (entityKey.match(heroRegex)) {
+        this.setFramesForEntityAnimations(entityValue, entityKey, heroAnims);
+      }
+      if (entityKey.match(scorpionRegex)) {
+        this.setFramesForEntityAnimations(entityValue, entityKey, scorpionAnims);
+      }
+    });
+  }
+
+  private setFramesForEntityAnimations(entityValue: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody, entityKey: string, entityAnims: Animations) {
+    this.createEntityAnimation.call(entityValue, "up-right", entityKey, entityAnims.walk.upRight.startFrame, entityAnims.walk.upRight.endFrame);
+    this.createEntityAnimation.call(entityValue, "down-right", entityKey, entityAnims.walk.downRight.startFrame, entityAnims.walk.downRight.endFrame);
+    this.createEntityAnimation.call(entityValue, "down-left", entityKey, entityAnims.walk.downLeft.startFrame, entityAnims.walk.downLeft.endFrame);
+    this.createEntityAnimation.call(entityValue, "up-left", entityKey, entityAnims.walk.upLeft.startFrame, entityAnims.walk.upLeft.endFrame);
   }
 
   createEntityAnimation(direction: string, entityName: string, startFrame: number, endFrame: number) {
@@ -211,16 +296,42 @@ class Game extends Phaser.Scene {
     });
   }
 
-  getStopFrame(direction: string): number {
+  getStopFrame(direction: string, entityKey: string): number {
+    const heroRegex = /^player/i;
+    const scorpionRegex = /^scorpion/i;
+
+    let entityAnims = {
+      walk: {
+        upRight: {
+          stopFrame: -1,
+        },
+        downRight: {
+          stopFrame: -1,
+        },
+        downLeft: {
+          stopFrame: -1,
+        },
+        upLeft: {
+          stopFrame: -1,
+        },
+      },
+    };
+    if (entityKey.match(heroRegex)) {
+      entityAnims = heroAnims;
+    }
+    if (entityKey.match(scorpionRegex)) {
+      entityAnims = scorpionAnims;
+    }
+
     switch (direction) {
       case "up-right":
-        return 0;
+        return entityAnims.walk.upRight.stopFrame;
       case "down-right":
-        return 16;
+        return entityAnims.walk.downRight.stopFrame;
       case "down-left":
-        return 24;
+        return entityAnims.walk.downLeft.stopFrame;
       case "up-left":
-        return 40;
+        return entityAnims.walk.upLeft.stopFrame;
       default:
         return -1;
     }
@@ -232,7 +343,7 @@ class Game extends Phaser.Scene {
     }
   }
 
-  tintTiles(map: Tilemaps.Tilemap){
+  tintTiles(map: Tilemaps.Tilemap) {
     this.tintTile(map, 30, 35, 0xff7a4a); // orange
     this.tintTile(map, 35, 28, 0xffff0a); // yellow
     this.tintTile(map, 30, 22, 0x4a4aff); // blue
