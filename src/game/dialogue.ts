b8935ee4215cb922e93f9ebd185d929aea638eac
Kirill <bloodwingest@gmail.com>
2023-02-20 20:13:43 +0300

    feat: add dynamic dialogue selection

--- src/game/dialogue.ts
+++ src/game/dialogue.ts
@@ -2,14 +2,19 @@ import Phaser, { Tilemaps } from 'phaser';
 import RexUIPlugin from 'phaser3-rex-plugins/templates/ui/ui-plugin.js';
 import { windowSize } from './constants'
 import config from './config'
-import {options} from './dialogue-states'
+import { options } from './dialogue-states'
+
+type DialogueKey = keyof typeof options;
+type StateKey = keyof typeof options[DialogueKey]
 
 class Dialogue extends Phaser.Scene {
   rexUI: RexUIPlugin;
+  currDialogue: DialogueKey;
 
   constructor(rexUI: RexUIPlugin) {
     super('dialogue')
     this.rexUI = rexUI;
+    this.currDialogue = 'dialogue-2'; //need to change this to change the dialogue
   }
 
   preload() {
@@ -17,9 +22,9 @@ class Dialogue extends Phaser.Scene {
     this.load.html('dialogue', '/assets/html/dialogue.html');
   }
 
-  create(){
+  create() {
     this.createDialogueTemplate(this)
-    this.setDialogueState(options['dialogue-1']['state-1'])
+    this.setDialogueState(options[this.currDialogue]['state-1'])
   }
 
   createDialogueTemplate(scene: Dialogue) {
@@ -28,35 +33,34 @@ class Dialogue extends Phaser.Scene {
     dialogue.scrollFactorY = 0;
   }
 
-  setDialogueState(state: { text: string; answers: string[][] }){
+  setDialogueState(state: { text: string; answers: string[][] }) {
     this.clearDialogueTexts();
     const text = document.querySelector('.text-container') as HTMLElement;
     const answerContainer = document.querySelector('.answers-list') as HTMLElement;
-    text.textContent = state.text!;
+    text.textContent = state.text;
 
     state.answers.forEach((el: string[]) => {
-      type ObjectKey = keyof typeof options['dialogue-1'];
       const answer = document.createElement('li');
-      const objKey = el[1] as ObjectKey;
+      const objKey = el[1] as StateKey;
 
       answer.classList.add('answer')
       answer.textContent = el[0];
 
-      if(el[1] === 'state-0'){
-        answer.addEventListener('click', () =>{
+      if (el[1] === 'state-0') {
+        answer.addEventListener('click', () => {
           this.sys.game.destroy(true)
           new Phaser.Game(config)
         })
       }
 
-      answer.addEventListener('click', () =>{
-        this.setDialogueState(options['dialogue-1'][objKey])
+      answer.addEventListener('click', () => {
+        this.setDialogueState(options[this.currDialogue][objKey])
       })
       answerContainer.appendChild(answer)
     })
   }
 
-  clearDialogueTexts(){
+  clearDialogueTexts() {
     const text = document.querySelector('.text-container') as HTMLElement;
     const answerContainer = document.querySelector('.answers-list') as HTMLElement;
 
