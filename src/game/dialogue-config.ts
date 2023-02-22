1bea5e31390dcf480f896fb3232a85f913f265ba
Kirill <bloodwingest@gmail.com>
2023-02-22 18:44:20 +0300

    fix: delete useless code

--- src/game/dialogue-config.ts
+++ src/game/dialogue-config.ts
@@ -1,33 +1,16 @@
 import { windowSize } from './constants';
-import GridEngine from 'grid-engine';
-import Game from './game';
 import Dialogue from './dialogue';
-import UIPlugin from 'phaser3-rex-plugins/templates/ui/ui-plugin.js';
 
 const dialogueConfig = {
-    type: Phaser.WEBGL,
-    width: windowSize.windowWidth,
-    height: windowSize.windowHeight,
-    dom: {
-      createContainer: true,
-    },
-    backgroundColor: '#000000',
-    parent: 'phaser-example',
-    scene: [Dialogue],
-    physics: {
-      default: 'arcade'
-    },
-    arcade: {
-      debug: true
-    },
-    plugins: {
-      scene: [{
-        key: 'rexUI',
-        plugin: UIPlugin,
-        mapping: 'rexUI'
-    },
-    ]
-    },
+  type: Phaser.WEBGL,
+  width: windowSize.windowWidth,
+  height: windowSize.windowHeight,
+  dom: {
+    createContainer: true,
+  },
+  backgroundColor: '#000000',
+  parent: 'phaser-example',
+  scene: [Dialogue],
 };
 
 export default dialogueConfig;
